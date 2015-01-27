ee0efad64a0e0c9f8fa0867cb3110c4f9b8b18e0
Manfredi <manfbenvenuti@gmail.com>
2015-01-27 15:51:14 +0100

    Messi i .env e Procfile

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,31 @@
+class PagesController < ApplicationController
+  
+  def home
+
+    # Per il Mail-Form
+    @contact = Contact.new
+
+  end
+  
+  def about
+  end
+
+  def h_mach
+  end
+
+  def vtl_mach
+  end
+
+  def igl_mach
+  end
+
+  def accessories
+  end
+
+  def contact
+  end
+
+  def specialties
+  end
+
+end
